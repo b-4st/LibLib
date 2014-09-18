9d3e6c0b3e7749c4c79206e4f55f69ac8d991480
DoktuhParadox <doktuhparadox@gmail.com>
2014-09-18 06:42:54 -0500

    Initial commit

---
+++ src/com/doktuhparadox/liblib/Main.java
@@ -0,0 +1,56 @@
+package com.doktuhparadox.liblib;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException, /*Won't happen*/ ParseException {
+        Scanner scanner = new Scanner(System.in); //The input scanner
+
+        System.out.print("Enter the lib to read.\n>"); //Prompts the user for a file path
+
+        while (true) {
+            File f = new File(scanner.nextLine());
+
+            if (f.exists()) { //Make sure the file exists
+                String[] rawLibWords = new BufferedReader(new FileReader(f)).lines().reduce((s, s2) -> s + "\n" + s2).get().split("\\s+"); //Stream all of the lines of the file into a string, then split it
+                Map<String, String> previousWords = new HashMap<>(4);
+
+                for (int i = 0; i < rawLibWords.length; i++) {
+                    String word = rawLibWords[i];
+
+                    if (word.matches("%\\w*(\\-\\d)?%(\\p{Punct}+)?")) { //It's a placeholder asking for a replacement
+                        String sanitizedPlaceholder = word.replace("_", " ").replaceAll("[^\\p{Alpha}&&[^\\s]]", ""); //Remove all non-alphabetic characters except spaces from the string
+                        System.out.printf("Enter a%s %s: ", "AEIOUaeiou".indexOf(sanitizedPlaceholder.charAt(0)) != -1 ? 'n' : "", sanitizedPlaceholder); //Prints the request for input, checking if it starts with a vowel
+                        String newWord = word.replaceAll("%\\w*(\\-\\d)?%", scanner.next()); //Replaces the word but preserves punctuation
+                        rawLibWords[i] = newWord; //Put it in the array
+                        previousWords.put(cleanedForMapKey(word), newWord); //Record this in the map in case a reference is made later
+                    } else if (word.matches("%\\$\\w*(\\-\\d)?%(\\p{Punct}+)?")) { //It's a placeholder asking for a word from the map
+                        if (previousWords.containsKey(cleanedForMapKey(word))) {
+                            rawLibWords[i] = previousWords.get(cleanedForMapKey(word)); //Put
+                        } else {
+                            throw new ParseException("Unresolved reference: " + word, i);
+                        }
+                    }
+                }
+
+                Arrays.asList(rawLibWords).forEach(s -> System.out.print(s.equals("$n") ? "\n" : s + " "));
+                break;
+            } else {
+                System.out.println("That file does not exist.");
+            }
+        }
+    }
+
+    private static String cleanedForMapKey(String s) {
+        return s.replaceAll("[\\p{Punct}&&[^\\-]]", "");
+    }
+}
